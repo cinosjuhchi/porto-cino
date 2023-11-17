bb1576c89f884c8db8dcddb201cf2a15c891253d
cinosjuhchi <pacino447@gmail.com>
2023-11-17 08:34:55 +0700

    first commit

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,11 @@
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a href="https://instagram.com/stormbreakkerrss" target="_blank" className="home__social-icon" rel="noreferrer"><i className="uil uil-instagram"></i></a>
+      <a href="https://github.com/cinosjuhchi" target="_blank" className="home__social-icon" rel="noreferrer"><i className="uil uil-github-alt"></i></a>
+      <a href="https://facebook.com/pacino.343" target="_blank" className="home__social-icon" rel="noreferrer"><i className="uil uil-facebook"></i></a>
+    </div>
+  );
+};
+
+export default Social;
